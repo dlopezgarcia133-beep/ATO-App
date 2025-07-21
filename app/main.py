c207ce7aa68a7abf8719734afe5354fe78f56904
Daniel Lopez Garcia <utm21040159@utma.edu.mx>
2025-07-21 11:56:51 -0600

    Cambio de direccion en el frontend

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ app.include_router(inventarioTelefonos.router, prefix="/inventario_telefonos", t
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["https://atosistema.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
