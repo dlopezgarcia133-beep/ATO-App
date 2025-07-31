f975de43bad9e0d5512a41b1dcc5f329b1c936db
Daniel Lopez Garcia <utm21040159@utma.edu.mx>
2025-07-31 12:08:46 -0600

    cambios en las asistencias

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,6 @@
 
 from datetime import datetime
+from zoneinfo import ZoneInfo
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
@@ -16,28 +17,27 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+zona_horaria_local = ZoneInfo("America/Mexico_City")
+
 @router.post("/token")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.Usuario).filter(models.Usuario.username == form_data.username).first()
     if not user or not pwd_context.verify(form_data.password, user.password):
         raise HTTPException(status_code=401, detail="Credenciales inválidas")
-    
-    
+
     if user.is_admin:
         token = crear_token({"sub": user.username, "rol": user.rol})
         return {"access_token": token, "token_type": "bearer"}
 
-    # Verifica si ya tiene asistencia registrada hoy
-    hoy = datetime.now().date()
+    ahora_local = datetime.now(tz=zona_horaria_local)
+    hoy = ahora_local.date()
+    
     asistencia_existente = db.query(models.Asistencia).filter(
         models.Asistencia.nombre == user.username,
         models.Asistencia.fecha == hoy
     ).first()
 
-
-
-    
-    def determinar_turno (hora: datetime.time) -> str:
+    def determinar_turno(hora):
         if hora >= datetime.strptime("08:00", "%H:%M").time() and hora < datetime.strptime("15:00", "%H:%M").time():
             return "mañana"
         elif hora >= datetime.strptime("15:00", "%H:%M").time() and hora < datetime.strptime("20:00", "%H:%M").time():
@@ -46,19 +46,19 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
             return "fuera de turno"
 
     if not asistencia_existente:
-        hora_actual = datetime.now().time()  # Hora actual del sistema
-        turno = determinar_turno(hora_actual) 
-        
+        turno = determinar_turno(ahora_local.time())
+
         nueva_asistencia = models.Asistencia(
             nombre=user.username,
-            modulo=user.modulo.nombre if user.modulo else None,  
-            turno= turno,
+            modulo=user.modulo.nombre if user.modulo else None,
+            turno=turno,
             fecha=hoy,
-            hora=datetime.now().time()
+            hora=ahora_local.time()
         )
         db.add(nueva_asistencia)
         db.commit()
         db.refresh(nueva_asistencia)
+
     token = crear_token({"sub": user.username, "rol": user.rol})
     return {"access_token": token, "token_type": "bearer"}
 
