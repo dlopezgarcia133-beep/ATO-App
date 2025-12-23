3d28b43f81198027fa67b7a66fbe5f72cedc0e37
Daniel Lopez <dlopezgarcia133@gmail.com>
2025-12-23 13:53:33 -0600

    cambio en base

--- app/models.py
+++ app/models.py
@@ -275,7 +275,7 @@ class NominaEmpleado(Base):
     id = Column(Integer, primary_key=True)
     usuario_id = Column(Integer, ForeignKey("usuarios.id"))
     periodo_id = Column(Integer, ForeignKey("nomina_periodo.id"))
-
+    grupo = Column(String(1), nullable=False)
     sueldo_base = Column(Float, default=0)
     horas_extra = Column(Float, default=0)
     pago_horas_extra = Column(Float, default=0)
